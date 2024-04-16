2798148f4e4d63c6ad6c9ddf0a2bc112515de010
StephenDK <stephendklein08@gmail.com>
2024-04-15 23:55:59 -0700

    Added Condition Display

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ $(document).ready(async function () {
     );
 
     locationDisplay(weatherData.location);
+    currentConditionDisplay(weatherData.current);
   } catch (err) {
     console.log(err);
   }
@@ -76,21 +77,6 @@ async function getAPIData(lat, long) {
   }
 }
 
-// Displays
-
-// Display for location data
-function locationDisplay({ ...location }) {
-  console.log("DATA", location.name);
-  var element = document.getElementById("location-name");
-  element.innerHTML = location.name;
-}
-
-// Display for today's weather data
-function todayDisplay() {}
-
-// Display for forcast weather data
-function forcastDisplay() {}
-
 // Initialize Map Display
 function addGoogleMapToElement(apiKey, latitude, longitude) {
   var mapUrl = `https://www.google.com/maps/embed/v1/view?key=${apiKey}&center=${latitude},${longitude}&zoom=12`;
@@ -107,3 +93,57 @@ function addGoogleMapToElement(apiKey, latitude, longitude) {
   var element = document.getElementById("google-map");
   element.appendChild(iframe);
 }
+
+// Display location data
+function locationDisplay({ ...location }) {
+  console.log("DATA", location.name);
+  var name = document.getElementById("location-name");
+  var country = document.getElementById("location-country");
+  var region = document.getElementById("location-region");
+  var localtime = document.getElementById("location-localtime");
+  var lat = document.getElementById("location-lat");
+  var lon = document.getElementById("location-lon");
+  name.innerHTML = location.name;
+  region.innerHTML = location.region;
+  country.innerHTML = location.country;
+  localtime.innerHTML = location.localtime;
+  lat.innerHTML = location.lat;
+  lon.innerHTML = location.lon;
+}
+
+// Display Today's Weather Data
+function currentConditionDisplay({ ...current }) {
+  var conditionText = document.getElementById("condition-text");
+  var temp_c = document.getElementById("temp_c");
+  var temp_f = document.getElementById("temp_f");
+  var condition_humidity = document.getElementById("condition-humidity");
+  var condition_precipitation = document.getElementById(
+    "condition-precipitation"
+  );
+  var condition_cloud = document.getElementById("condition-cloud");
+
+  conditionText.innerHTML = current.condition.text;
+  temp_c.innerHTML = current.temp_c;
+  temp_f.innerHTML = current.temp_f;
+  condition_humidity.innerHTML = current.humidity;
+  condition_precipitation.innerHTML = current.precip_in;
+  condition_cloud.innerHTML = current.cloud;
+}
+
+// Display atmosphere weather data
+function atmosphereDisplay({ ...current }) {
+  var windDirection = document.getElementById("current-windDirection");
+  var windMPH = document.getElementById("current-windMPH");
+  var windKPH = document.getElementById("current-windKPH");
+  var air_carbon = document.getElementById("air-quality-carbon");
+  var air_nitrogen = document.getElementById("air-quality-nitrogen");
+
+  windDirection.innerHTML = 
+  windMPH.innerHTML = 
+  windKPH.innerHTML = 
+  air_carbon.innerHTML = 
+  air_nitrogen.innerHTML = 
+}
+
+// Display for forcast weather data
+function forcastDisplay() {}
